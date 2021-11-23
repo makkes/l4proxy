cfdb403b183f59755f550f13b915a68063995502
Max Jonas Werner <mail@makk.es>
2021-11-23 22:31:04 +0100

    feat: add Frontend.Stop; avoid race conditions
    * Frontend.Start now returns and start the listener in a goroutine.
    * Frontend.Stop stops the frontend and all goroutines that Start
      started.
    * Backend health checking is now thread-safe.

--- config/config.go
+++ config/config.go
@@ -17,18 +17,18 @@ type Config struct {
 	// APIVersion specifies the version of the configuration file used when it was persisted. This field doesn't
 	// have any impact at the moment but provides forward-compatibility when a backwards-incompatible change has
 	// to be applied to the configuration in which case this version is increased.
-	APIVersion APIVersion `yaml:"apiVersion"`
-	Frontends  []Frontend `yaml:"frontends"`
+	APIVersion APIVersion `yaml:"apiVersion" json:"apiVersion"`
+	Frontends  []Frontend `yaml:"frontends" json:"frontends"`
 }
 
 type Frontend struct {
-	Bind           string    `yaml:"bind"`
-	Backends       []Backend `yaml:"backends"`
-	HealthInterval int       `yaml:"healthInterval"`
+	Bind           string    `yaml:"bind" json:"bind"`
+	Backends       []Backend `yaml:"backends" json:"backends"`
+	HealthInterval int       `yaml:"healthInterval" json:"healthInterval"`
 }
 
 type Backend struct {
-	Address string `yaml:"address"`
+	Address string `yaml:"address" json:"address"`
 }
 
 func Read(cfgPath string) (*Config, error) {
