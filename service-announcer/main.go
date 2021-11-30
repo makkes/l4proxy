f50eb789055e2750ee1ae45cbe8cc4930c883954
Max Jonas Werner <mail@makk.es>
2021-11-30 17:59:03 +0100

    feat: add service announcer command
    This command is a Kubernetes controller that watches Services and
    creates an associated l4proxy configuration file from the LoadBalancer
    ones.
    
    This controller is meant to be run either outside of Kubernetes so
    that l4proxy can access the configuration or save the configuration
    file to a volume mounted through e.g. NFS.

---
+++ service-announcer/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/go-logr/logr"
+	l4proxyconfig "github.com/makkes/l4proxy/config"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type Reconciler struct {
+	client         client.Client
+	healthInterval int
+	logger         logr.Logger
+	l4ProxyConfig  string
+	bind           string
+}
+
+func (r Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	var svc corev1.Service
+	log := r.logger
+	if err := r.client.Get(ctx, req.NamespacedName, &svc); err != nil {
+		if !errors.IsNotFound(err) {
+			return reconcile.Result{}, err
+		}
+	} else {
+		log = log.WithValues("namespace", svc.GetNamespace(), "name", svc.GetName())
+		if svc.Spec.Type != corev1.ServiceTypeLoadBalancer {
+			log.Info("skipping non-LoadBalancer service")
+			return reconcile.Result{}, nil
+		}
+	}
+
+	var svcs corev1.ServiceList
+	if err := r.client.List(ctx, &svcs); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	lbs := make(map[string][]int32)
+
+	for _, svc := range svcs.Items {
+		if svc.DeletionTimestamp != nil && !svc.DeletionTimestamp.IsZero() {
+			continue
+		}
+		for _, ingress := range svc.Status.LoadBalancer.Ingress {
+			if _, ok := lbs[ingress.IP]; !ok {
+				lbs[ingress.IP] = make([]int32, 0)
+			}
+			for _, port := range svc.Spec.Ports {
+				if port.Protocol == "TCP" {
+					lbs[ingress.IP] = append(lbs[ingress.IP], port.Port)
+				}
+			}
+		}
+	}
+
+	cfg := l4proxyconfig.Config{
+		APIVersion: l4proxyconfig.APIVersionV1,
+	}
+	for ip, ports := range lbs {
+		for _, port := range ports {
+			fe := l4proxyconfig.Frontend{
+				Bind: fmt.Sprintf("%s:%d", r.bind, port),
+				Backends: []l4proxyconfig.Backend{
+					{
+						Address: fmt.Sprintf("%s:%d", ip, port),
+					},
+				},
+				HealthInterval: r.healthInterval,
+			}
+			cfg.Frontends = append(cfg.Frontends, fe)
+		}
+	}
+
+	out, err := os.OpenFile(r.l4ProxyConfig, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("could not open output file: %w", err)
+	}
+	defer out.Close()
+
+	encoder := yaml.NewEncoder(out)
+	encoder.SetIndent(2)
+	if err := encoder.Encode(cfg); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed marshalling config: %w", err)
+	}
+
+	log.Info("updated configuration file")
+
+	return reconcile.Result{}, nil
+}
+
+func (r *Reconciler) InjectClient(c client.Client) error {
+	r.client = c
+	return nil
+}
+
+func (r *Reconciler) InjectLogger(l logr.Logger) error {
+	r.logger = l
+	return nil
+}
+
+func main() {
+	var (
+		l4ProxyConfig string
+		bind          string
+		setupLog      = ctrl.Log.WithName("setup")
+	)
+
+	zap.New()
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
+		MetricsBindAddress: "0",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	flags := flag.NewFlagSet("main", flag.ExitOnError)
+	flags.StringVar(&l4ProxyConfig, "l4proxy-config", "", "The path of the l4proxy config file.")
+	flags.StringVar(&bind, "bind", "", "The address that l4proxy will bind to")
+	flags.Parse(os.Args[1:])
+
+	if l4ProxyConfig == "" {
+		setupLog.Error(fmt.Errorf("l4proxy config file not set"), "--l4proxy-config cannot be empty")
+		os.Exit(1)
+	}
+
+	err = builder.ControllerManagedBy(mgr).
+		For(&corev1.Service{}).
+		Complete(&Reconciler{
+			l4ProxyConfig:  l4ProxyConfig,
+			bind:           bind,
+			healthInterval: 5,
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := mgr.Start(context.TODO()); err != nil {
+		panic(err)
+	}
+}
